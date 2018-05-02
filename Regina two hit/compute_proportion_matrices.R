cbf2311b5145aa03ba9c72eea1c42eee673b0ab7
kgostic <kategostic@gmail.com>
2018-05-02 14:53:50 -0700

    Updated the twohit gete_ij function, and the compute proportion matrices function. Last task is pseudocode assignment

--- Regina two hit/compute_proportion_matrices.R
+++ Regina two hit/compute_proportion_matrices.R
@@ -1,126 +1,186 @@
-source('regina_pt3code.R')
+source('gete_ij_twohit.R')
+## Inputs:
+##    -> b_yr: Scalar. Birth year of interest
+##    -> i_yr: Scalar. Year in which case was observed
+##    -> flu_circ_ch: ???? Data on fraction of influenza A circulation caused by H1N1, H2N2 or H3N2 in the year of interest.
+
+
+## Outputs:
+## One list with the following elements
+##  -> H1p_11: probs of exposure to any H1 virus @ 1st and second exposures
+##  -> H1p_10: probs of exposure to any H1 virus @ 1st exposure, some other virus @ 2nd
+##  -> H1p_01: probs of exposure to any H1 virus @ 2nd exposure, some other virus @ 1st
+##  -> H1p_00: probs of exposure to a virus other than H1 @ 1st and 2nd exposures
+##  -> H1p_1n: probs of exposure to any H1 virus @ 1st exposure and 2nd exposure hasn't yet occurred
+##  -> H1p_on: probs of exposure to any virus other than H1 @ 1st exposure and 2nd exposure hasn't yet occurred
+##  -> H2p_11: ... same as above for any H2 virus
+##  -> H2p_10
+##  -> H2p_01
+##  -> H2p_00
+##  -> H2p_1n
+##  -> H2p_on
+##  -> H3p_11 ... same as above for any H3 virus
+##  -> H3p_10
+##  -> H3p_01
+##  -> H3p_00
+##  -> H3p_1n
+##  -> H3p_on
+##  -> p_nn:  probs of no first or second exposure yet (naive to influenza) 
+
 
 get_proportion = function(b_yr, i_yr, flu_circ_ch){
-   # flu_circ_ch = read.csv('China_flu_circulation.csv'); colnames(flu_circ_ch) = c('',2017:1901); rownames(flu_circ_ch) = c('H1','H2','H3','Group1','Group2');
-   # rownames(flu_circ_ch) <- flu_circ_ch[,1]
-   # flu_circ_ch <- flu_circ_ch[,-1]
-  # 
-  ## Use existing function to output probabilities that people born in 1995 had their first and second exposures in 1995, 1996, 1997...etc.
-  func_result= get.e_ij(birth.year = b_yr, incidence.year = i_yr) # Tuple
-  prob.matrix = func_result$total_mat
-  p_nn = func_result$totalPof_noexp1 # same as no_exp1
-  no_exp2 = func_result$totalPof_yesexp1_and_noexp2
+  func_result= get.e_ij(birth.year = b_yr, incidence.year = i_yr) # Outputs the probabilities of a first exposure in year i and second exposure in year j, as well as the probabilites of no first exposure, and of a first exposure in year i, but no second exposure.
+  prob.matrix = func_result$total_mat # matrix: row = year of 1st exposure, column = year of 2nd exposure
+  p_nn = func_result$totalPof_noexp1 # scalar
+  no_exp2 = func_result$totalPof_yesexp1_and_noexp2 # vector: element = year of 1st exposure.
   
-  nn = length(b_yr:min(b_yr+17, i_yr))
-  naive = matrix(0L,nrow=nn, ncol=nn) #incident year successfully starts with naive = 1
+  nn = length(b_yr:min(b_yr+17, i_yr)) ## total number of years of possible first exposure years
   
   # Store probs of naive in 2nd exposure
-  g1p_1n = g1p_0n = g2p_1n = g2p_0n = vector('numeric', nn)
-  over_max_age = FALSE
+  H1p_1n = H1p_0n = H2p_1n = H2p_0n = H3p_0n = H3p_1n = vector('numeric', nn)
+  over_max_age = 0
   
-  ## Normalize so prob.matrix sums to 1
-  if(i_yr - b_yr > 17){ # This birth cohorts is currently over the max age
+  if(i_yr - b_yr > 17){ # If the birth cohorts is over age 17 at the time of observation
+    # Assume everyone has had both a first and second exposure
+    # Normalize the matrix so that probabilities sum to 1, this implicitly sets naive probs to 0
     prob.matrix = prob.matrix/sum(prob.matrix, na.rm = TRUE)
     over_max_age = 18
-  #}else{ # This birth cohort is currently under the minumum age
-    #naive = 1 - sum(prob.matrix, na.rm = TRUE)
-  }
-  ## Calculate probs of first and second exposure for H1N1 and H2N2 (Group 1)
-    g1p_11 = g1p_10 = g1p_01 = g1p_00 = matrix(nrow=nn, ncol=nn)
-    
+  } # Else, assume some individuals are still missing an first or second exposure, and calculate the naive fraction below
+  
+  ## Calculate probs of first and second exposure for H1N1
+    H1p_11 = H1p_10 = H1p_01 = H1p_00 = matrix(nrow=nn, ncol=nn) # empty matrices
     for(exp1 in 1:nn){ # point at a specific year index in vector exp1
       mm = nn - exp1 # length of all poss exp2 years
-      
-      e1_g1 = flu_circ_ch[4,toString(exp1+b_yr-1)] # Probability of that year for H1 AND H2
+      e1_H1 = flu_circ_ch[1,toString(exp1+b_yr-1)] # Extract fraction of all flu A circulation caused by group 1 in the year of first exposure
       if(mm>0){
         for(exp2 in (1+exp1):(mm+exp1)) { # Point at specific year index in vector exp2
-          e2_g1 = flu_circ_ch[4,toString(exp2+b_yr-1)]
-          
-          #prob.matrix supposed to give exp to ANY strain
-          g1p_11[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_g1) * (e2_g1)
-          g1p_10[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_g1) * (1-e2_g1)
-          g1p_01[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_g1) * (e2_g1)
-          g1p_00[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_g1) * (1-e2_g1)
+          e2_H1 = flu_circ_ch[1,toString(exp2+b_yr-1)] # Extract fraction of all flu A circulation caused by group 1 in the year of second exposure
+          #prob.matrix gives prob of any [exp1, exp2] combination of years
+          #multiply by the probabilities of group 1 exposure in those years to calculate the probability of first and/or second exposure to a specific HA group
+          H1p_11[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_H1) * (e2_H1)
+          H1p_10[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_H1) * (1-e2_H1)
+          H1p_01[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_H1) * (e2_H1)
+          H1p_00[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_H1) * (1-e2_H1)
         }
       }
       if(over_max_age == 0){
-        g1p_1n[exp1] = (e1_g1 * (no_exp2[exp1])) # wmn overall prob of g1 imprinting on 1st exp
-        g1p_0n[exp1] = ((1-e1_g1) * (no_exp2[exp1])) # won overall prob of g1 imprinting on 1st exp
+        H1p_1n[exp1] = (e1_H1 * (no_exp2[exp1])) # wmn overall prob of H1 imprinting on 1st exp, but no second exposure
+        H1p_0n[exp1] = ((1-e1_H1) * (no_exp2[exp1])) # won overall prob of H1 imprinting on 1st exp, but no second exposure
       }
       else{
-        over_max_age= over_max_age - 1; # decrease over_max_age
-        g1p_1n[exp1] = 0
-        g1p_0n[exp1] = 0
+        H1p_1n[exp1] = 0
+        H1p_0n[exp1] = 0
         p_nn = 0
       }
-    } # End loop over years of 1st exposure
+    } # End loop for H1N1
    
-  #########################################################################
     
-  ## Calculate probs of first and second exposure for H2N2 (updated version already implies this within Group1)
-    # 
-    # H2p_11 = H2p_10 = H2p_01 = H2p_00 = matrix(nrow=nn, ncol=nn)
-    # for(exp1 in 1:nn){ # point at a specific year index in vector exp1
-    #   mm = nn - exp1 #length of all poss exp2 years
-    # 
-    #   e1_h2n2 = flu_circ_ch[2,toString(exp1+b_yr-1)] #constant, obtain prob of that year
-    #   if(mm>0){
-    #     for(exp2 in (1+exp1):(mm+exp1)) { # point at a specific year index in vector exp2
-    #       e2_h2n2 = flu_circ_ch[2,toString(exp2+b_yr-1)]
-    # 
-    #       #prob.matrix supposed to give exp to ANY strain
-    #       H2p_11[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_h2n2) * (e2_h2n2)
-    #       H2p_10[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_h2n2) * (1-e2_h2n2)
-    #       H2p_01[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_h2n2) * (e2_h2n2)
-    #       H2p_00[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_h2n2) * (1-e2_h2n2)
-    #     }
-    #   }
-    # }
-
-  ##########################################################################
-  ## Calculate probs of first and second exposure for H3N2 (Group 2)
-    # reset no_exp1 and counter
-    p_nn = func_result$totalPof_noexp1
-    if(i_yr - b_yr > 17){ # This birth cohorts is currently over the max age
+    
+    ########## Repeat for H2N2 childhood exposures
+    # Store probs of naive in 2nd exposure
+    over_max_age = 0
+    
+    if(i_yr - b_yr > 17){ # If the birth cohorts is over age 17 at the time of observation
+      # Assume everyone has had both a first and second exposure
+      # Normalize the matrix so that probabilities sum to 1, this implicitly sets naive probs to 0
       prob.matrix = prob.matrix/sum(prob.matrix, na.rm = TRUE)
       over_max_age = 18
-    }
+    } # Else, assume some individuals are still missing an first or second exposure, and calculate the naive fraction below
     
-    g2p_11 = g2p_10 = g2p_01 = g2p_00 = matrix(nrow=nn, ncol=nn)
-    
+    ## Calculate probs of first and second exposure for H2N1
+    H2p_11 = H2p_10 = H2p_01 = H2p_00 = matrix(nrow=nn, ncol=nn) # empty matrices
     for(exp1 in 1:nn){ # point at a specific year index in vector exp1
       mm = nn - exp1 # length of all poss exp2 years
-      e1_g2 = flu_circ_ch[5,toString(exp1+b_yr-1)] # constant, obtain prob of that year
+      e1_H2 = flu_circ_ch[2,toString(exp1+b_yr-1)] # Extract fraction of all flu A circulation caused by group 1 in the year of first exposure
       if(mm>0){
-        for(exp2 in (1+exp1):(mm+exp1)) { # point at a specific year index in vector exp2
-          e2_g2 = flu_circ_ch[5,toString(exp2+b_yr-1)]
-          
-          g2p_11[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_g2) * (e2_g2)
-          g2p_10[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_g2) * (1-e2_g2)
-          g2p_01[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_g2) * (e2_g2)
-          g2p_00[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_g2) * (1-e2_g2)
-        } # End loop over years of second exposure
+        for(exp2 in (1+exp1):(mm+exp1)) { # Point at specific year index in vector exp2
+          e2_H2 = flu_circ_ch[2,toString(exp2+b_yr-1)] # Extract fraction of all flu A circulation caused by group 1 in the year of second exposure
+          #prob.matrix gives prob of any [exp1, exp2] combination of years
+          #multiply by the probabilities of group 1 exposure in those years to calculate the probability of first and/or second exposure to a specific HA group
+          H2p_11[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_H2) * (e2_H2)
+          H2p_10[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_H2) * (1-e2_H2)
+          H2p_01[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_H2) * (e2_H2)
+          H2p_00[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_H2) * (1-e2_H2)
+        }
       }
       if(over_max_age == 0){
-        g2p_1n[exp1] = (e1_g2 * (no_exp2[exp1])) # wmn overall prob of g2 imprinting on 1st exp
-        g2p_0n[exp1] = ((1-e1_g2) * (no_exp2[exp1])) # won overall prob of g2 imprinting on 1st exp
+        H2p_1n[exp1] = (e1_H2 * (no_exp2[exp1])) # wmn overall prob of H2 imprinting on 1st exp, but no second exposure
+        H2p_0n[exp1] = ((1-e1_H2) * (no_exp2[exp1])) # won overall prob of H2 imprinting on 1st exp, but no second exposure
       }
       else{
-        over_max_age= over_max_age - 1; # decrease over_max_age
-        g2p_1n[exp1] = 0
-        g2p_0n[exp1] = 0
+        H2p_1n[exp1] = 0
+        H2p_0n[exp1] = 0
         p_nn = 0
       }
-    } # End loop over years of 1st exposure
+    } # End loop for H2N2
     
     
     
-    #result <- list("naive" = naive, "H1p_11" = H1p_11, "H1p_10" = H1p_10, "H1p_01" = H1p_01, "H1p_00" = H1p_00, "H2p_11" = H2p_11, "H2p_10" = H2p_10, "H2p_01" = H2p_01, "H2p_00" = H2p_00, "H3p_11" = H3p_11, "H3p_10" = H3p_10, "H3p_01" = H3p_01, "H3p_00" = H3p_00)
-    #result <- list("naive" = naive, "g1p_11" = g1p_11, "g1p_10" = g1p_10, "g1p_01" = g1p_01, "g1p_00" = g1p_00, "g2p_11" = g2p_11, "g2p_10" = g2p_10, "g2p_01" = g2p_01, "g2p_00" = g2p_00)
-    result <- list("g1p_11" = g1p_11, "g1p_10" = g1p_10, "g1p_01" = g1p_01, "g1p_00" = g1p_00, "g1p_1n" = g1p_1n, "g1p_0n" = g1p_0n, "g2p_11" = g2p_11, "g2p_10" = g2p_10, "g2p_01" = g2p_01, "g2p_00" = g2p_00, "g2p_1n" = g2p_1n, "g2p_0n" = g2p_0n, "p_nn" = p_nn)
+    
+    
+    # Store probs of naive in 2nd exposure
+    over_max_age = 0
+    
+    if(i_yr - b_yr > 17){ # If the birth cohorts is over age 17 at the time of observation
+      # Assume everyone has had both a first and second exposure
+      # Normalize the matrix so that probabilities sum to 1, this implicitly sets naive probs to 0
+      prob.matrix = prob.matrix/sum(prob.matrix, na.rm = TRUE)
+      over_max_age = 18
+    } # Else, assume some individuals are still missing an first or second exposure, and calculate the naive fraction below
+    
+    ## Calculate probs of first and second exposure for H3N2
+    H3p_11 = H3p_10 = H3p_01 = H3p_00 = matrix(nrow=nn, ncol=nn) # empty matrices
+    for(exp1 in 1:nn){ # point at a specific year index in vector exp1
+      mm = nn - exp1 # length of all poss exp2 years
+      e1_H3 = flu_circ_ch[3,toString(exp1+b_yr-1)] # Extract fraction of all flu A circulation caused by group 1 in the year of first exposure
+      if(mm>0){
+        for(exp2 in (1+exp1):(mm+exp1)) { # Point at specific year index in vector exp2
+          e2_H3 = flu_circ_ch[3,toString(exp2+b_yr-1)] # Extract fraction of all flu A circulation caused by group 1 in the year of second exposure
+          #prob.matrix gives prob of any [exp1, exp2] combination of years
+          #multiply by the probabilities of group 1 exposure in those years to calculate the probability of first and/or second exposure to a specific HA group
+          H3p_11[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_H3) * (e2_H3)
+          H3p_10[exp1,exp2] = prob.matrix[exp1, exp2] * (e1_H3) * (1-e2_H3)
+          H3p_01[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_H3) * (e2_H3)
+          H3p_00[exp1,exp2] = prob.matrix[exp1, exp2] * (1-e1_H3) * (1-e2_H3)
+        }
+      }
+      if(over_max_age == 0){
+        H3p_1n[exp1] = (e1_H3 * (no_exp2[exp1])) # wmn overall prob of H3 imprinting on 1st exp, but no second exposure
+        H3p_0n[exp1] = ((1-e1_H3) * (no_exp2[exp1])) # won overall prob of H3 imprinting on 1st exp, but no second exposure
+      }
+      else{
+        H3p_1n[exp1] = 0
+        H3p_0n[exp1] = 0
+        p_nn = 0
+      }
+    } # End loop for H3N2
+    
+    
+# return
+    result <- list("H1p_00" = H1p_00, "H1p_01" = H1p_01, "H1p_0n" = H1p_0n, "H1p_10" = H1p_10, "H1p_11" = H1p_11, "H1p_1n" = H1p_1n, "H2p_00" = H2p_00, "H2p_01" = H2p_01, "H2p_0n" = H2p_0n, "H2p_10" = H2p_10, "H2p_11" = H2p_11, "H2p_1n" = H2p_1n, "H3p_00" = H3p_00, "H3p_01" = H3p_01, "H3p_0n" = H3p_0n,  "H3p_10" = H3p_10, "H3p_11" = H3p_11, "H3p_1n" = H3p_1n, "p_nn" = p_nn)
     result
 }
 
 
-#sum(p_11, na.rm = TRUE)+sum(p_10, na.rm = TRUE) +sum(p_01, na.rm = TRUE) +sum(p_00, na.rm = TRUE)
-#sum(prob.matrix, na.rm = TRUE)=.8650201
+
+
+## Test
+# source('country_data_import.R')
+# flu_circ_input = import.country.dat(cc)
+# ## Test for a birth year and observation year less than 17 years apart
+# Tests below should sum to 1
+# test = get_proportion(b_yr = 2000, i_yr = 2009, flu_circ_ch = flu_circ_input)
+# attach(test)
+# sum(H1p_11, na.rm = T)+sum(H1p_10, na.rm = T)+sum(H1p_01, na.rm = T)+sum(H1p_00, na.rm = T)+sum(H1p_1n, na.rm = T)+sum(H1p_0n, na.rm = T)+sum(p_nn)
+# sum(H2p_11, na.rm = T)+sum(H2p_10, na.rm = T)+sum(H2p_01, na.rm = T)+sum(H2p_00, na.rm = T)+sum(H2p_1n, na.rm = T)+sum(H2p_0n, na.rm = T)+sum(p_nn)
+# sum(H3p_11, na.rm = T)+sum(H3p_10, na.rm = T)+sum(H3p_01, na.rm = T)+sum(H3p_00, na.rm = T)+sum(H3p_1n, na.rm = T)+sum(H3p_0n, na.rm = T)+sum(p_nn)
+# detach(test)
+# 
+# ## Test for a birth year and observation year more than 17 years apart
+# Tests below should sum to 1
+# test = get_proportion(b_yr = 1981, i_yr = 2009, flu_circ_ch = flu_circ_input)
+# attach(test)
+# sum(H1p_11, na.rm = T)+sum(H1p_10, na.rm = T)+sum(H1p_01, na.rm = T)+sum(H1p_00, na.rm = T)+sum(H1p_1n, na.rm = T)+sum(H1p_0n, na.rm = T)+sum(p_nn)
+# sum(H2p_11, na.rm = T)+sum(H2p_10, na.rm = T)+sum(H2p_01, na.rm = T)+sum(H2p_00, na.rm = T)+sum(H2p_1n, na.rm = T)+sum(H2p_0n, na.rm = T)+sum(p_nn)
+# sum(H3p_11, na.rm = T)+sum(H3p_10, na.rm = T)+sum(H3p_01, na.rm = T)+sum(H3p_00, na.rm = T)+sum(H3p_1n, na.rm = T)+sum(H3p_0n, na.rm = T)+sum(p_nn)
+# detach(test)
--- Regina two hit/gete_ij_twohit.R
+++ Regina two hit/gete_ij_twohit.R
@@ -1,42 +1,33 @@
-#setwd("/Users/reginalee/Dropbox/Classical Disease Papers/Research scripts/Regina_research/")
-#TIP: press TAB for setwd()
-#Check that you set your working directory correctly
-getwd()
-
-## Note - Intensitymaster.csv contains data loaded into this function
-
 ## Function inputs the year in which an individual was born (birth.year), and in which the individual became infected with bird flu (incidence year)
 
-## Function outputs a vector of 13 probabilities, the first representing the probability of first flu infection in the first year of life (age 0), the second representing the probability of first flu infection in the second year of life (age 1), and so on up to the 18th year of life (age 17)
+## Function outputs a matrix of probabilities, the first representing the probability of first flu infection in the first year of life (age 0), the second representing the probability of first flu infection in the second year of life (age 1), and so on up to the 18th year of life (age 17)
+# Output a list, where the first entry gives the original total matrix, the second entry gives the total prob of having no first exposure and the third entry gives the total prob of a first exposure, but no second exposure.
 
-## Outputs correspond to the numerator of equation 3 in the supplementary methods
-
-## Function is called by another script (Infection_age_structure.R), which calculates normalized probabilities (see denominators in equations 3 and 4), and calculates probability of first exposure to a specific HA subtype.
+## Function is called by another script which calculates normalized probabilities (see denominators in equations 3 and 4), and calculates probability of first exposure to a specific HA subtype.
 
 get.e_ij = function(birth.year, incidence.year){
   ## Inputs
   #Load saved data on intensity of influenza circulation in specific years of first infection
-  intensities = read.csv('Intensitymatser.csv', col.names = c('Year', 'Intensity')); rownames(intensities) = 1911:2017
-  load('pest.RData') # Load the annual probability of first infection, estimated from serological data (see two papers by Sauerbrei et al.)
-  #TIP: save results as lists in Rdata file, check when workspace is empty
-  
-  # Weighted attack rate = annual prob infection weighted by circulation intensity
-  weighted.attack.rate = p.est*(intensities$Intensity); names(weighted.attack.rate) = 1911:2017
+  intensities = read.csv('../Intensitymatser.csv', col.names = c('Year', 'Intensity')); rownames(intensities) = 1911:2017
+ # load('pest.RData') # Load the annual probability of first infection, estimated from serological data (see two papers by Sauerbrei et al.)
+
+  # Weighted attack rate = annual prob infection scaled by historical data on year-specific circulation intensity (from Intensitymaster.csv)
+  weighted.attack.rate = 0.28*(intensities$Intensity); names(weighted.attack.rate) = 1911:2017
   
   ## Calculations (vector, constant, vector)
   jjs = birth.year:min(birth.year+17, incidence.year) #Possible years of first infection (ages 0-17)
   nn = length(jjs) # Number of possible years of 1st inf.
   ajs = weighted.attack.rate[as.character(jjs)] #Get weighted attack rates "p" corresponding to possible years of first infection
-  #names(e_ij) = jjs; names(naiive) = jjs
   
   
   #### BEGIN CALCULATION OF PROBABILITIES OF EXPOSURE 1 AND EXPOSURE 2 AT A TIME 0, 1, 2, ... 17 YEARS AFTER BIRTH.
   ## Let i represent row index and j represent column index.
-  ## entry total_ij gives the total probability of having a first exposure in year i and a second exposure in year j
-  total <- matrix(NA, nrow=nn, ncol=nn); #want to be a MATRIX
-  ## KG addition:
+  ## entry total_ij gives the probability of having a first exposure in year i and a second exposure in year j
+  total <- matrix(NA, nrow=nn, ncol=nn)
+
   # all_exp1_probs is a vector that stores the total probability of 1st exposure in year exp1, regardless of exp2 status.
-  all_exp1_probs = vector('numeric', nn)
+  # The complement gives the probabilitiy of no first exposure (needed below).
+  all_exp1_probs = vector('numeric', nn) # Initialize empty vector
   
   # OUTER LOOP--CALCULATE THE PROBABILITY OF FIRST EXPOSURE IN EACH POSSIBLE YEAR FROM 0-17 YEARS AFTER BIRTH
   for(exp1 in 1:nn){ #year is, increment "0-1" "0-2" "0-3 etc", CONSTANT length nn
@@ -47,6 +38,8 @@ get.e_ij = function(birth.year, incidence.year){
       # Then we don't need any factors of (1-p). Store a 1, so we can multiply by 1*p below.
       years.not.infected1 = 1 #1-ajs[1] (multiplication purposes)
     }
+    # Multiplying years.not.infected1[exp1]*ajs[exp1] gives the total prob of first exposure in year exp1
+    
     # 2. Calculate and store the total probabilitiy of first exposure in year exp1
     all_exp1_probs[exp1] = years.not.infected1*ajs[exp1]
     
@@ -68,13 +61,12 @@ get.e_ij = function(birth.year, incidence.year){
       }## Exit if statement for >0 possible years of 2nd exposure
     }## Exit inner loop: 2nd exposure calculation
   }## Exit outter loop: 1st exposure calculation
-  ## Return total
-  #return(list('total_mat' = total, 'totalPof_noexp1' = 1-sum(all_exp1_probs), 'totalPof_yesexp1_and_noexp2' = sum(all_exp1_probs)-sum(total, na.rm = TRUE)))
   return(list('total_mat' = total, 'totalPof_noexp1' = 1-sum(all_exp1_probs), 'totalPof_yesexp1_and_noexp2' = all_exp1_probs-rowSums(total, na.rm = TRUE)))
 } # End function
 
-## KG - 
-# Now the function outputs a list, where the first entry gives the original total matrix, the second entry gives the total prob of having no first exposure and the third entry gives the total prob of a first exposure, but no second exposure.
 
-
-#0.23235532 0.10578127 0.33370429 0.05259924 0.08451051 0.05644345 from unnested version
+# ## check:
+# get.e_ij(2009, 2017)
+# test = get.e_ij(2000, 2009)
+# # Sum should equal 1
+# sum(test$total_mat, na.rm = T)+sum(test$totalPof_noexp1)+sum(test$totalPof_yesexp1_and_noexp2)
